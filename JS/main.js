ac01e6b89d31f80b9d086c7cd56b45c3b510b22b
Autumn Raine <autumn@inerdgirl.com>
2019-06-24 13:06:55 -0600

    Finished game logic 😇

--- JS/main.js
+++ JS/main.js
@@ -10,8 +10,101 @@ const scoreboard = {
 
 //Play Game
 function play(e) {
-	console.log(e.target.id);
+	restart.style.display = 'inline-block';
+	const playerChoice = e.target.id;
+	const computerChoice = getComputerChoice();
+	const winner = getWinner(playerChoice, computerChoice);
+
+    showWinner(winner, computerChoice);
+}
+
+//Get computer choice
+function getComputerChoice() {
+	const rand = Math.random();
+	if (rand < 0.34) {
+		return 'rock';
+	} else if (rand <= 0.67) {
+		return 'paper';
+	} else {
+		return 'scissors';
+	}
+}
+
+//Get game winner
+function getWinner(p, c) {
+	if (p === c) {
+		return 'draw';
+	} else if (p === 'rock') {
+		if (c === 'paper') {
+			return 'computer';
+		} else {
+			return 'player';
+		}
+	} else if (p === 'paper') {
+		if (c === 'scissors') {
+			return 'computer';
+		} else {
+			return 'player';
+		}
+	} else if (p === 'scissors') {
+		if (c === 'rock') {
+			return 'computer';
+		} else {
+			return 'player';
+		}
+	}
+}
+
+//Show winner
+function showWinner(winner, computerChoice) {
+	if (winner === 'player') {
+		scoreboard.player++;
+		result.innerHTML = `
+            <h1 class="text-win">You Win!</h1>
+            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer chose <strong>${computerChoice}</strong></p>
+        `;
+	} else if (winner === 'computer') {
+		scoreboard.computer++;
+		result.innerHTML = `
+            <h1 class="text-lose">You Lose!</h1>
+            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer chose <strong>${computerChoice}</strong></p>
+        `;
+	} else {
+		result.innerHTML = `
+            <h1>It's a draw</h1>
+            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer chose <strong>${computerChoice}</strong></p>
+        `;
+	}
+
+	score.innerHTML = `
+        <p>Player: ${scoreboard.player}</p>
+        <p>Computer: ${scoreboard.computer}</p>
+    `;
+
+	modal.style.display = 'block';
+}
+
+//Restart Game 
+function restartGame() {
+    scoreboard.player = 0;
+    scoreboard.computer = 0;
+    score.innerHTML = `
+        <p>Player: 0</p>
+        <p>Computer: 0</p>
+    `;
+}
+
+//Clear modal
+function clearModal(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
 }
 
 //Event listeners
 choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
